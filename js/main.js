1d2ae7b31adf49f71e474d4bdd7e168ba79fd852
Karen0731 <karenbolanos0731@gmail.com>
2021-04-28 16:37:03 -0500

    Added first gravity anim, removed ammo

--- js/main.js
+++ js/main.js
@@ -6,71 +6,119 @@ import * as Ammo from '/js/jsm/physics/ammo.js';
 
 "using strict";
 
-let renderer, scene, camera, mesh, cameraControl, stats;
+let renderer, scene, camera, mesh, cameraControl, stats, gui;
+window.anim = false;
+var targetPositionY = 0.5;
+var gravity = 0.01;
+var velocity = 0.1; 
 
-        function init() {
+function init() {
 
-            // RENDERER
-            renderer = new THREE.WebGLRenderer({antialias: true});
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            document.body.appendChild(renderer.domElement);
+    // RENDERER
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.BasicShadowMap;
+    document.body.appendChild(renderer.domElement);
 
-            // SCENE
-            scene = new THREE.Scene();
+    // SCENE
+    scene = new THREE.Scene();
 
-            // CAMERA
-            let fov = 60;
-            let aspect = window.innerWidth / window.innerHeight;
-            let near = 0.1;
-            let far = 10000;
-            camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-            camera.position.set(0, 0, 3);
-            cameraControl  = new OrbitControls(camera, renderer.domElement);
+    // CAMERA
+    let fov = 60;
+    let aspect = window.innerWidth / window.innerHeight;
+    let near = 0.1;
+    let far = 10000;
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(2, 5, 10);
+    cameraControl  = new OrbitControls(camera, renderer.domElement);
 
-            // MODELS
-            let geometry = new THREE.BoxGeometry();
-            let material = new THREE.MeshBasicMaterial({color: "white", wireframe: true});
-            mesh = new THREE.Mesh(geometry, material);
-            mesh.name= "Cube";
+    // LIGHTS
+    let directionalLight = new THREE.DirectionalLight();
+    directionalLight.position.set(5, 3, 0);
+    directionalLight.castShadow = true;
 
-            // SCENE GRAPH
-            scene.add(mesh);
+    // MODELS
+    let geometry = new THREE.BoxGeometry();
+    let material = new THREE.MeshPhongMaterial({color: "red"});
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0,3,0);
+    mesh.castShadow = true;
+    mesh.name= "Cube";
 
-            //GUI   
-            let gui = new dat.GUI();
+    //FLOOR
+    let floor = new Floor();
+    floor.receiveShadow = true;
+
+    // SCENE GRAPH
+    scene.add(mesh);
+    scene.add(floor);
+    scene.add(directionalLight);
+
+    //GUI
+    gui = new dat.GUI();
+    gui.add(window, "anim").name("Start Simulation").listen().onChange(function(value) {
+
+    });
+    gui.open();
 
 
-            gui.close();
+    gui.close();
 
-            //STATS
-            stats = new Stats();
-            stats.showPanel(0);
-            document.body.appendChild(stats.dom);
+    //STATS
+    stats = new Stats();
+    stats.showPanel(0);
+    document.body.appendChild(stats.dom);
 
-            // RENDER LOOP
-            renderLoop();
+    // RENDER LOOP
+    renderLoop();
+}
+
+function renderLoop() {
+    stats.begin();
+    renderer.render(scene, camera); // DRAW SCENE
+    updateScene();
+    stats.end();
+    stats.update();
+    requestAnimationFrame(renderLoop);
+}
+
+
+function updateScene() 
+{
+    //FIRST ANIMATION FOR GRAVITY 
+    if(anim) {
+        if (mesh.position.y <= targetPositionY) {
+             velocity = -velocity * 0.84; 
+             //console.log("reversed");
         }
-
-        function renderLoop() {
-            stats.begin();
-            renderer.render(scene, camera); // DRAW SCENE
-            updateScene();
-            stats.end();
-            stats.update();
-            requestAnimationFrame(renderLoop);
+        else{
+            velocity += gravity;
+            //console.log(velocity);
         }
+        mesh.position.y -= velocity;
+    }
+}
 
-        
-        function updateScene() 
-        {
+// EVENT LISTENERS & HANDLERS
+document.addEventListener("DOMContentLoaded", init);
 
-        }
+window.addEventListener("resize", function(){
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
 
-        // EVENT LISTENERS & HANDLERS
-        document.addEventListener("DOMContentLoaded", init);
-
-        window.addEventListener("resize", function(){
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            camera.aspect = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix();
-        });
+// MODELS
+class Floor extends THREE.Mesh {
+    constructor() {
+        super();
+        this.geometry = new THREE.PlaneGeometry(20, 20, 20, 20);
+        this.material = new THREE.MeshPhongMaterial();
+        this.rotation.x = -0.5 * Math.PI;
+        this.wireframeHelper = new THREE.LineSegments(new THREE.WireframeGeometry(this.geometry));
+        this.wireframeHelper.material.color = new THREE.Color(0.2, 0.2, 0.2);
+        this.add(this.wireframeHelper);
+        this.visible = true;
+    }
+}
